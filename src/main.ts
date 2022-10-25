0a7ea06f68ed225d7d4308381a47a161474f9ec3
Süleyman Furkan Binay <suleymanfurkanbinay@Suleyman-MacBook-Air.local>
2022-10-25 23:15:46 +0300

    project init, typescript and eslint config enabled

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+
+const port = 3000;
+
+app.get("/", function (req, res) {
+  res.json({
+    test: "Test",
+  });
+});
+
+app.listen(port, () => {
+  //eslint-disable-next-line
+  console.log("Server starting at port: 3000");
+});
