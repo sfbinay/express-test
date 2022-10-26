e162ad14096ec0ee33491cf9936cc64a50931d12
Süleyman Furkan Binay <suleymanfurkanbinay@Suleyman-MacBook-Air.local>
2022-10-26 22:45:13 +0300

    prettier  enabled

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,8 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  extends: ["plugin:@typescript-eslint/recommended"],
+  extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+  plugins: ["@typescript-eslint/eslint-plugin", "prettier"],
   env: {
     node: true,
   },
@@ -12,5 +13,10 @@ module.exports = {
     "no-console": "warn",
     "import/prefer-default-export": "off",
     "@typescript-eslint/no-unused-vars": "warn",
+    "prettier/prettier": ["error"],
+    "no-multi-spaces": "error",
+    "space-in-parens": "error",
+    "no-multiple-empty-lines": "error",
+    "prefer-const": "error",
   },
 };
